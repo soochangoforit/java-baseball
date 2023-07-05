7210a0528bcd4c8438c4ee1f8f709681910a4b31
soochan <tncksdl05@gmail.com>
2023-07-05 18:23:00 +0900

    feat(OutputView): 사용자에게 입력 받기 위한 코멘트 출력 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -14,9 +14,14 @@ public class OutputView {
 
 
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String SCAN_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
 
     public void printGameStart() {
         System.out.println(GAME_START_MESSAGE);
+    }
+
+    public void printScanNumberMessage() {
+        System.out.println(SCAN_NUMBER_MESSAGE);
     }
 
 }
