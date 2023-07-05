a00b04f556de6a9dfb995398cd59f3cc0771439b
soochan <tncksdl05@gmail.com>
2023-07-05 18:37:35 +0900

    refactor(OutputView): 입력을 받는 멘트를 InputView로 위임

---
+++ src/main/java/baseball/view/InputView.java
@@ -0,0 +1,24 @@
+package baseball.view;
+
+
+public class InputView {
+
+    private InputView() { }
+
+    public static InputView getInstance() {
+        return LazyHolder.INSTANCE;
+    }
+
+    private static class LazyHolder {
+        private static final InputView INSTANCE = new InputView();
+    }
+
+
+    private static final String SCAN_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+
+    public void printScanNumberMessage() {
+        System.out.print(SCAN_NUMBER_MESSAGE);
+    }
+
+
+}
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -14,14 +14,10 @@ public class OutputView {
 
 
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
-    private static final String SCAN_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
 
     public void printGameStart() {
         System.out.println(GAME_START_MESSAGE);
     }
 
-    public void printScanNumberMessage() {
-        System.out.print(SCAN_NUMBER_MESSAGE);
-    }
 
 }
