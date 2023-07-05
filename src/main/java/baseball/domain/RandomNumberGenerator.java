c0093126d4b40b1e48a491b0e769a24a10c8ca32
soochan <tncksdl05@gmail.com>
2023-07-05 22:21:34 +0900

    refactor(RandomNumberGenerator): 응답형식을 Set에서 List으로 변경

--- src/main/java/baseball/domain/RandomNumberGenerator.java
+++ src/main/java/baseball/domain/RandomNumberGenerator.java
@@ -2,19 +2,21 @@ package baseball.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class RandomNumberGenerator {
 
-    public static Set<Integer> generateRandomNumber() {
+    public static List<Integer> generateRandomNumber() {
         Set<Integer> randomNumbers = new HashSet<>();
         while (randomNumbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             randomNumbers.add(randomNumber);
         }
 
-        return randomNumbers;
+        return new ArrayList<>(randomNumbers);
     }
 
 }
--- src/test/java/baseball/domain/RandomNumberGeneratorTest.java
+++ src/test/java/baseball/domain/RandomNumberGeneratorTest.java
@@ -11,7 +11,7 @@ class RandomNumberGeneratorTest {
 
     @Test
     void _1에서_9까지_범위의_중복되지_않는_랜덤_숫자를_발행한다() {
-        Set<Integer> randomNumbers = RandomNumberGenerator.generateRandomNumber();
+        List<Integer> randomNumbers = RandomNumberGenerator.generateRandomNumber();
 
         assertThat(randomNumbers).hasSize(3);
         assertThat(randomNumbers).allMatch(number -> number >= 1 && number <= 9);
@@ -20,7 +20,7 @@ class RandomNumberGeneratorTest {
     // TODO : 새롭게 배운 doesNotHaveDuplicates()
     @Test
     void _생성된_랜덤한_숫자는_중복되지_않는다() {
-        Set<Integer> randomNumbers = RandomNumberGenerator.generateRandomNumber();
+        List<Integer> randomNumbers = RandomNumberGenerator.generateRandomNumber();
 
         assertThat(randomNumbers).hasSize(3);
         assertThat(randomNumbers).doesNotHaveDuplicates();
