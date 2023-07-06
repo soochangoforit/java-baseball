9effab54fb6cb351c3315475eaba4098bc93333a
soochan <tncksdl05@gmail.com>
2023-07-06 19:25:28 +0900

    test(RandomNumberGeneratorTest): 랜덤한 숫자 발생하는 로직 변경으로 인한 테스트 코드 수정

--- src/test/java/baseball/domain/RandomNumberGeneratorTest.java
+++ src/test/java/baseball/domain/RandomNumberGeneratorTest.java
@@ -10,11 +10,10 @@ class RandomNumberGeneratorTest {
     private final Generator randomNumberGenerator = new RandomNumberGenerator();
 
     @Test
-    void _generateRandomNumber은_BaseBallNumbers_타입을_반환한다() {
-        BaseBallNumbers randomNumbers = randomNumberGenerator.generateRandomNumber();
+    void _generateRandomNumber은_1_9사이의_숫자를_반환한다() {
+        int randomNumber = randomNumberGenerator.generateRandomNumber();
 
-        assertThat(randomNumbers.size()).isEqualTo(3);
-        assertThat(randomNumbers).isInstanceOf(BaseBallNumbers.class);
+        assertThat(randomNumber).isBetween(1, 9);
     }
 
     // TODO : 새롭게 배운 doesNotHaveDuplicates()
