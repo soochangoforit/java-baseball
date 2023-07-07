da15feb41ce647355c5b3e45c7ce1a709e25e4c9
soochan <tncksdl05@gmail.com>
2023-07-07 15:51:09 +0900

    feat(InputView): 게임을 지속할건지 물어보는 메서드 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -5,24 +5,17 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class InputView {
 
+    private static final String SCAN_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+
     private InputView() { }
-
-    public static InputView getInstance() {
-        return LazyHolder.INSTANCE;
-    }
-
-    public boolean askKeepPlaying() {
-        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        String input = Console.readLine();
-        return input.equals("1");
-    }
 
     private static class LazyHolder {
         private static final InputView INSTANCE = new InputView();
     }
 
-
-    private static final String SCAN_NUMBER_MESSAGE = "숫자를 입력해주세요 : ";
+    public static InputView getInstance() {
+        return LazyHolder.INSTANCE;
+    }
 
     public String printScanNumberMessage() {
         System.out.print(SCAN_NUMBER_MESSAGE);
@@ -31,5 +24,9 @@ public class InputView {
         return inputNumbers;
     }
 
-
+    public boolean askKeepPlaying() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        String input = Console.readLine();
+        return input.equals("1");
+    }
 }
