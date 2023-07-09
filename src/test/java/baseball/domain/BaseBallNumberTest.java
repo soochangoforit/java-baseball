9dc4e97bfcf3b1900858715120958a33744489a7
soochan <tncksdl05@gmail.com>
2023-07-09 12:16:54 +0900

    test(BaseBallNumberTest): 숫자 1 ~ 9를 감싸기 위한 Wrapper 클래스에 대한 테스트 코드 추가

---
+++ src/test/java/baseball/domain/BaseBallNumberTest.java
@@ -0,0 +1,27 @@
+package baseball.domain;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+class BaseBallNumberTest {
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 10})
+    void BaseBallNumber는_1에서_9사이_숫자가_아니면_Exception_반환(int baseBallNumber) {
+        assertThatThrownBy(() -> new BaseBallNumber(baseBallNumber))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 9})
+    void BaseBallNumber는_1에서_9사이_숫자면_Exception_반환하지_않는다(int baseBallNumber) {
+        assertDoesNotThrow(() -> new BaseBallNumber(baseBallNumber));
+    }
+
+
+
+}
