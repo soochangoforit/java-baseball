71489d5006a5c8d9da9cf3bde90e8d1b7b5b646e
soochan <tncksdl05@gmail.com>
2023-07-09 11:24:56 +0900

    test(InputValidator): 사용자의 입력 길이가 3과 같은 경우는 exception 반환하지 않는 테스트 추가

--- src/test/java/baseball/view/InputValidatorTest.java
+++ src/test/java/baseball/view/InputValidatorTest.java
@@ -3,7 +3,9 @@ package baseball.view;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 
 class InputValidatorTest {
 
@@ -12,5 +14,11 @@ class InputValidatorTest {
     void validatePlayerBaseBallNumber메서드는_입력값의_길이가_3과_맞지_않으면_Exception반환(String playNumbers) {
         assertThatThrownBy(() -> InputValidator.validatePlayerBaseBallNumber(playNumbers))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"123", "456", "789"})
+    void validatePlayerBaseBallNumber메서드는_입력값의_길이가_3과_맞으면_입력값을_반환하지_않는다(String playNumbers) {
+        assertDoesNotThrow(() -> InputValidator.validatePlayerBaseBallNumber(playNumbers));
     }
 }
