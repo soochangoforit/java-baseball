821f14146b1aae449462575e47998e967d32e2bb
soochan <tncksdl05@gmail.com>
2023-07-09 11:40:38 +0900

    feat(InputValidator): 사용자 입력 값이 문자인 경우에는 Exception 응답

--- src/main/java/baseball/view/InputValidator.java
+++ src/main/java/baseball/view/InputValidator.java
@@ -5,11 +5,16 @@ public class InputValidator {
     private static final int BASEBALL_NUMBER_SIZE = 3;
 
     public static String validatePlayerBaseBallNumber(String playerBaseBallNumber) {
-        if (!validateNumberSize(playerBaseBallNumber)) {
+        if (!validateNumberSize(playerBaseBallNumber) || !validateNumber(playerBaseBallNumber)) {
             throw new IllegalArgumentException();
         }
 
         return playerBaseBallNumber;
+    }
+
+    private static boolean validateNumber(String playerBaseBallNumber) {
+        return playerBaseBallNumber.chars()
+                .allMatch(Character::isDigit);
     }
 
     private static boolean validateNumberSize(String playerBaseBallNumber) {
--- src/test/java/baseball/view/InputValidatorTest.java
+++ src/test/java/baseball/view/InputValidatorTest.java
@@ -21,4 +21,11 @@ class InputValidatorTest {
     void validatePlayerBaseBallNumber메서드는_입력값의_길이가_3과_맞으면_입력값을_반환하지_않는다(String playNumbers) {
         assertDoesNotThrow(() -> InputValidator.validatePlayerBaseBallNumber(playNumbers));
     }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"abc", "d12", "g"})
+    void validatePlayerBaseBallNumber메서드는_입력값이_숫자가_아니면_Exception반환(String playNumbers) {
+        assertThatThrownBy(() -> InputValidator.validatePlayerBaseBallNumber(playNumbers))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
