ea27b4a2e2351ce03a89f33772c86c3c60229f0c
soochan <tncksdl05@gmail.com>
2023-07-09 11:11:54 +0900

    feat(InputValidator): 사용자 입력의 크기가 맞지 않은 경우 exception 반환

--- src/main/java/baseball/view/InputValidator.java
+++ src/main/java/baseball/view/InputValidator.java
@@ -2,8 +2,17 @@ package baseball.view;
 
 public class InputValidator {
 
+    private static final int BASEBALL_NUMBER_SIZE = 3;
+
     public static String validatePlayerBaseBallNumber(String playerBaseBallNumber) {
+        if (!validateNumberSize(playerBaseBallNumber)) {
+            throw new IllegalArgumentException();
+        }
 
         return playerBaseBallNumber;
+    }
+
+    private static boolean validateNumberSize(String playerBaseBallNumber) {
+        return playerBaseBallNumber.length() == BASEBALL_NUMBER_SIZE;
     }
 }
