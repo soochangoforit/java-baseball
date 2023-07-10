9a94fc7aa6502ccd3a10b8f3b065c59374e74a9c
soochan <tncksdl05@gmail.com>
2023-07-10 15:37:33 +0900

    feat(OutputView): 게임 결과를 출력하는 메서드 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,10 +1,29 @@
 package baseball.view;
 
+import baseball.domain.BaseBallGameResult;
+
 public class OutputView {
 
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
 
     private OutputView() { }
+
+    public void printGameResult(BaseBallGameResult gameResult) {
+        if (gameResult.hasNothing()) {
+            System.out.println("낫싱");
+        }
+        if (gameResult.hasBall()) {
+            int ballCount = gameResult.getBallCount();
+            System.out.printf("%d볼 ", ballCount);
+        }
+        if (gameResult.hasStrike()) {
+            int strikeCount = gameResult.getStrikeCount();
+            System.out.printf("%d스트라이크\n", strikeCount);
+        }
+        if (gameResult.hasAllStrike()) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+    }
 
     private static class LazyHolder {
         private static final OutputView INSTANCE = new OutputView();
