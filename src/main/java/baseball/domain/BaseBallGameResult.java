b045c18c5af27ddb07690b17a4706e9eb1eedca8
soochan <tncksdl05@gmail.com>
2023-07-10 15:36:28 +0900

    feat(BaseBallGameResult): 게임 결과 상태를 판단할 수 있는 메서드 추가

--- src/main/java/baseball/domain/BaseBallGameResult.java
+++ src/main/java/baseball/domain/BaseBallGameResult.java
@@ -10,4 +10,32 @@ public class BaseBallGameResult {
         this.strike = strike;
         this.ball = ball;
     }
+
+    public BaseBallGameResult(int strikeCount, int ballCount) {
+        this(Strike.from(strikeCount), Ball.from(ballCount));
+    }
+
+    public boolean hasBall() {
+        return ball.hasBall();
+    }
+
+    public int getBallCount() {
+        return ball.getBallCount();
+    }
+
+    public boolean hasStrike() {
+        return strike.hasStrike();
+    }
+
+    public int getStrikeCount() {
+        return strike.getStrikeCount();
+    }
+
+    public boolean hasNothing() {
+        return !hasBall() && !hasStrike();
+    }
+
+    public boolean hasAllStrike() {
+        return strike.isThreeStrike();
+    }
 }
---
+++ src/test/java/baseball/domain/BaseBallGameResultTest.java
@@ -0,0 +1,66 @@
+package baseball.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class BaseBallGameResultTest {
+
+    @Test
+    void hasBall메서드는_볼을_가지고_있는_경우_true를_응답한다() {
+        BaseBallGameResult baseBallGameResult = new BaseBallGameResult(1, 1);
+
+        assertTrue(baseBallGameResult.hasBall());
+    }
+
+    @Test
+    void hasBall메서드는_볼을_가지고_있지_않는_경우_false을_응답한다() {
+        BaseBallGameResult baseBallGameResult = new BaseBallGameResult(1, 0);
+
+        assertFalse(baseBallGameResult.hasBall());
+    }
+
+
+    @Test
+    void hasStrike메서드는_스트라이크을_가지고_있는_경우_true을_응답한다() {
+        BaseBallGameResult baseBallGameResult = new BaseBallGameResult(1, 1);
+
+        assertTrue(baseBallGameResult.hasStrike());
+    }
+
+    @Test
+    void hasStrike메서드는_스트라이크을_가지고_있지_않는_경우_true을_응답한다() {
+        BaseBallGameResult baseBallGameResult = new BaseBallGameResult(0, 1);
+
+        assertFalse(baseBallGameResult.hasStrike());
+    }
+
+    @Test
+    void hasNothing메서드는_볼과스트라이크_그_어느것도_가지지_않는_경우_true을_응답한다() {
+        BaseBallGameResult baseBallGameResult = new BaseBallGameResult(0, 0);
+
+        assertTrue(baseBallGameResult.hasNothing());
+    }
+
+    @Test
+    void hasNothing메서드는_볼과스트라이크_하나라도_가지는_경우_true을_응답한다() {
+        BaseBallGameResult baseBallGameResult = new BaseBallGameResult(1, 0);
+
+        assertFalse(baseBallGameResult.hasNothing());
+    }
+
+    @Test
+    void hasAllStrike메서드는_3개의_스트라이크를_가지는_경우_true을_응답한다() {
+        BaseBallGameResult baseBallGameResult = new BaseBallGameResult(3, 0);
+
+        assertTrue(baseBallGameResult.hasAllStrike());
+    }
+
+    @Test
+    void hasAllStrike메서드는_3개의_스트라이크를_가지지_않는_경우_false을_응답한다() {
+        BaseBallGameResult baseBallGameResult = new BaseBallGameResult(2, 0);
+
+        assertFalse(baseBallGameResult.hasAllStrike());
+    }
+}
