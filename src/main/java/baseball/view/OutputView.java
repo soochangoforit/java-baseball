9361614a785eea71441f937a9c09bc4ed6bbae53
soochan <tncksdl05@gmail.com>
2023-07-13 23:16:53 +0900

    Refactor(OutputView): 메시지 하드코딩을 상수값을 활용하도록 변경

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -5,37 +5,42 @@ import baseball.domain.BaseBallGameResult;
 public class OutputView {
 
     private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String NOTHING_MESSAGE = "낫싱";
+    private static final String BALL_AND_STRIKE_MESSAGE = "%d볼 %d스트라이크\n";
+    private static final String BALL_ONLY_MESSAGE = "%d볼\n";
+    private static final String STRIKE_ONLY_MESSAGE = "%d스트라이크\n";
+    private static final String ALL_STRIKE_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
     private OutputView() {
     }
 
     public void printGameResult(BaseBallGameResult gameResult) {
         if (gameResult.hasNothing()) {
-            System.out.println("낫싱");
+            System.out.println(NOTHING_MESSAGE);
             return;
         }
 
         if (gameResult.hasBall() && gameResult.hasStrike()) {
             int ballCount = gameResult.getBallCount();
             int strikeCount = gameResult.getStrikeCount();
-            System.out.printf("%d볼 %d스트라이크\n", ballCount, strikeCount);
+            System.out.printf(BALL_AND_STRIKE_MESSAGE, ballCount, strikeCount);
             return;
         }
 
         if (gameResult.hasBall() && !gameResult.hasStrike()) {
             int ballCount = gameResult.getBallCount();
-            System.out.printf("%d볼\n", ballCount);
+            System.out.printf(BALL_ONLY_MESSAGE, ballCount);
             return;
         }
+
         if (gameResult.hasStrike() && !gameResult.hasBall()) {
             int strikeCount = gameResult.getStrikeCount();
-            System.out.printf("%d스트라이크\n", strikeCount);
+            System.out.printf(STRIKE_ONLY_MESSAGE, strikeCount);
+
             if (gameResult.hasAllStrike()) {
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                System.out.println(ALL_STRIKE_MESSAGE);
             }
         }
-
-
     }
 
     private static class LazyHolder {
@@ -49,6 +54,5 @@ public class OutputView {
     public void printGameStartMessage() {
         System.out.println(GAME_START_MESSAGE);
     }
-
 
 }
