c503ac7438929b9f97b343d7226993ddc5c47565
soochan <tncksdl05@gmail.com>
2023-07-13 18:30:56 +0900

    refactor(BaseBallGameTest): BaseBallGameTest 필요 없는 테스트 제거

--- src/test/java/baseball/domain/BaseBallGameTest.java
+++ src/test/java/baseball/domain/BaseBallGameTest.java
@@ -2,7 +2,6 @@ package baseball.domain;
 
 import static baseball.domain.Ball.NONE;
 import static baseball.domain.Strike.THREE;
-import static org.assertj.core.api.Assertions.assertThat;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -10,12 +9,6 @@ import org.junit.jupiter.api.Test;
 
 class BaseBallGameTest {
 
-    @Test
-    void initializeGame메서드로_BaseBallGame을_생성하는_경우_기본_게임상태는_NOT_FINISH이다() {
-        BaseBallGame baseBallGame = BaseBallGame.initializeGame(new BaseBallNumbers(1, 2, 3));
-
-        assertThat(baseBallGame.isFinished()).isFalse();
-    }
 
     @Test
     void startGame메서드는_게임을_진행하고_모두_맞춘경우_3스타라이크_결과를_반환한다() {
@@ -26,23 +19,5 @@ class BaseBallGameTest {
         Assertions.assertThat(baseBallGameResult).isEqualTo(new BaseBallGameResult(THREE, NONE));
     }
 
-    @Test
-    void startGame메서드는_게임을_진행하고_모두_맞춘_경우_종료상태가_된다() {
-        BaseBallGame baseBallGame = BaseBallGame.initializeGame(new BaseBallNumbers(1, 2, 3));
-
-        baseBallGame.startGame(new BaseBallNumbers(1, 2, 3));
-
-        assertThat(baseBallGame.isFinished()).isTrue();
-    }
-
-    @Test
-    void isNotFinished메서드는_2스트라이크라서_아직_끝나지_않은_경우라면_true를_반환한다() {
-        BaseBallGame baseBallGame = BaseBallGame.initializeGame(new BaseBallNumbers(1, 2, 3));
-
-        baseBallGame.startGame(new BaseBallNumbers(1, 2, 4));
-
-        assertThat(baseBallGame.isNotFinished()).isTrue();
-    }
-  
 
 }
